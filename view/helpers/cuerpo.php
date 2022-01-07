e91edc32d87f54958ecbe9c9070146921fa5b9f2
Canos13 <canomsergio13@gmail.com>
2022-01-07 09:04:41 -0600

    se cambiaron las condiciones en la etiqueta <A/>

--- view/helpers/cuerpo.php
+++ view/helpers/cuerpo.php
@@ -9,7 +9,7 @@
                 <img src="view/images/escuela.png">
             <?php } else { ?>
                     <h1>Bienvenido <?php echo $username ?></h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere unde, sed similique in ipsa consequuntur quia asperiores laudantium expedita inventore minus beatae tempore ipsum error nulla at voluptatibus illo totam</p>
+                    <p>parrafo de bienvenida parrafo de bienvenida parrafo de bienvenida</p>
                 </div>
                 <img src="../images/escuela.png">
             <?php } ?>
@@ -25,27 +25,38 @@
 
         <section class="Two">
             <div class="info border1">
-                <?php  if(!isset($_SESSION['user_id'])){ ?>
-                    <img src="view/images/base-de-datos.png" class="temp">
-                <?php } else { ?>
-                    <img src="../images/base-de-datos.png" class="temp">
-                <?php } ?>
+                <img 
+                    src=<?php  
+                            if(!isset($_SESSION['user_id'])){
+                                echo "view/images/base-de-datos.png";
+                            } else {
+                                    echo "../images/base-de-datos.png";
+                            } ?>
+                    class="temp"
+                >
                 <h3>Consulta datos</h3>
             </div>
+
             <div class="info border2">
-                <?php  if(!isset($_SESSION['user_id'])){ ?>
-                    <img src="view/images/base-de-datos1.png" class="temp">
-                <?php } else { ?>
-                    <img src="../images/base-de-datos1.png" class="temp">
-                <?php } ?>
+                <img src=<?php  
+                            if(!isset($_SESSION['user_id'])){
+                                echo "view/images/base-de-datos1.png";
+                            } else {
+                                    echo "../images/base-de-datos1.png";
+                            } ?>
+                     class="temp"
+                >
                 <h3>Ingresa datos</h3>
             </div>
+
             <div class="info border3">
-                <?php  if(!isset($_SESSION['user_id'])){ ?>
-                    <img src="view/images/sencillo-formato-de-datos.png" class="temp">
-                <?php } else { ?>
-                    <img src="../images/sencillo-formato-de-datos.png" class="temp">
-                <?php } ?>
-                <h3>Ve estadisticas</h3>
+                <img src=<?php 
+                            if(!isset($_SESSION['user_id'])){
+                                echo "view/images/sencillo-formato-de-datos.png";
+                            } else {
+                                    echo "../images/sencillo-formato-de-datos.png";
+                            } ?>
+                     class="temp">
+                <h3>Ve estadísticas</h3>
             </div>
         </section>
