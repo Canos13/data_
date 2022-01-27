05e55d8b904761263d44c8d45ff111143cbaa674
Canos13 <canomsergio13@gmail.com>
2022-01-26 22:50:04 -0600

    creamos el archivo section

---
+++ view/html/section.php
@@ -0,0 +1,9 @@
+<?php require "../helpers/cabecera.php"; ?>
+<?php require "../../model/BD.php"; ?>
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Ver sección</title>
+<?php 
+    session_start();
+    $band = 1;
+    require "../helpers/navbar.php";  
+?>
