2a030b63d0a15f0d62244c56c1512a01f26275bf
Canos13 <canomsergio13@gmail.com>
2022-02-07 12:33:39 -0600

    inicio con las fs para obtener la info de la bd

--- view/html/section.php
+++ view/html/section.php
@@ -1,9 +1,27 @@
 <?php require "../helpers/cabecera.php"; ?>
 <?php require "../../model/BD.php"; ?>
+<?php require "../../model/Categoria.php"; ?>
     <link rel="stylesheet" href="../css/styles.css">
     <title>Ver sección</title>
 <?php 
     session_start();
     $band = 1;
     require "../helpers/navbar.php";  
+
+    $bd = new BD();
+    $categoria = new Categoria();
+
+    $categoria = $bd->consultarCategorias();
+    foreach($categoria as $cate){
+
+        $numColTablaActual = $bd->numColumnas(($cate->getName()));
+        echo $numColTablaActual.'<br />';
+        /* printf(" %s ", $resultado->getName()); */
+    }
+
+
+    /* $num = $bd->numTablasEnBD(); 
+    echo $num;*/
+
+    
 ?>
