bf036997c8583cc1d53e62e1ee4a296b65b38a71
Canos13 <canomsergio13@gmail.com>
2022-02-08 01:19:52 -0600

    about

---
+++ view/html/about.php
@@ -0,0 +1,5 @@
+<?php include "../helpers/cabecera.php" ?>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/Main.css">
+    <title>Acerca de</title>
+<?php include "../helpers/navbar.php" ?>
