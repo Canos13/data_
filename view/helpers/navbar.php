3aed82fae735a9c229d5d7a6a780bf1926ef0d3b
Canos13 <canomsergio13@gmail.com>
2022-02-23 20:15:43 -0600

    ruta del index

--- view/helpers/navbar.php
+++ view/helpers/navbar.php
@@ -14,7 +14,7 @@
                             if(isset($_SESSION['user_id'])){ 
                                 echo "home.php"; 
                             } else { 
-                                    echo "/data_";
+                                    echo "index.php";
                                 }  
                           ?> 
                 >Inicio</a>
